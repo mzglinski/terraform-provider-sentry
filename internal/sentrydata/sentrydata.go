54c30004616dbe2e1adfc809a7a485d89c6c0a37
mzglinski <23341314+mzglinski@users.noreply.github.com>
2025-09-24 01:56:31 +0000

    Update Sentry data

--- internal/sentrydata/sentrydata.go
+++ internal/sentrydata/sentrydata.go
@@ -199,7 +199,7 @@ var DashboardWidgetTypes = []string{
 	"logs",
 }
 
-// https://github.com/getsentry/sentry/blob/master/src/sentry/models/dashboard_widget.py#L137-L154
+// https://github.com/getsentry/sentry/blob/master/src/sentry/models/dashboard_widget.py#L141-L158
 var DashboardWidgetDisplayTypes = []string{
 	"line",
 	"area",
